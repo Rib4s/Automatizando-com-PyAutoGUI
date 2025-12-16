1760217eb7e8cb46ddefea39660432f3955b52c3
Rib4s <paulo-ribas@live.com>
2025-12-16 15:39:43 -0300

    Módulo 03 - Identificando componentes na tela

--- 01_funcoes_teclado.py
+++ 01_funcoes_teclado.py
@@ -1,36 +1,36 @@
 import pyautogui
 
-# pyautogui.typewrite('# Ola Mundo!\n', interval=0.05) # escreve algo na tela, o intervalo define a velocidade da escrita
-# # pyautogui.write('Ola Mundo!', interval=0.10) (uso não convencional)
+pyautogui.typewrite('# Ola Mundo!\n', interval=0.05) # escreve algo na tela, o intervalo define a velocidade da escrita
+# pyautogui.write('Ola Mundo!', interval=0.10) (uso não convencional)
 
-# pyautogui.press('enter') # pressiona uma tecla definida na função
-# pyautogui.press('enter')
-# pyautogui.press('enter')
+pyautogui.press('enter') # pressiona uma tecla definida na função
+pyautogui.press('enter')
+pyautogui.press('enter')
 
-# # Combinação de tecla pressionada
-# pyautogui.keyDown('shift')
-# pyautogui.typewrite('# ribas')
-# pyautogui.keyUp('shift', '\n')
+# Combinação de tecla pressionada
+pyautogui.keyDown('shift')
+pyautogui.typewrite('# ribas')
+pyautogui.keyUp('shift', '\n')
 
-# # Atalhos de teclas
-# pyautogui.hotkey('alt', 'tab')
-# pyautogui.hotkey('ctrl', 'shift', 'esc')
-# pyautogui.hotkey('alt', 'tab')
-# pyautogui.hotkey('alt', 'tab')
+# Atalhos de teclas
+pyautogui.hotkey('alt', 'tab')
+pyautogui.hotkey('ctrl', 'shift', 'esc')
+pyautogui.hotkey('alt', 'tab')
+pyautogui.hotkey('alt', 'tab')
 
-# # Função sleep
-# pyautogui.typewrite('# Boa\n')
-# pyautogui.sleep(2)
-# pyautogui.typewrite('# Noite\n')
+# Função sleep
+pyautogui.typewrite('# Boa\n')
+pyautogui.sleep(2)
+pyautogui.typewrite('# Noite\n')
 
-# # Função pause
-# pyautogui.PAUSE = 2
-# pyautogui.typewrite('# Bom\n')
-# pyautogui.typewrite('# Dia\n')
-# pyautogui.hotkey('ctrl', 'shift', 'esc')
+# Função pause
+pyautogui.PAUSE = 2
+pyautogui.typewrite('# Bom\n')
+pyautogui.typewrite('# Dia\n')
+pyautogui.hotkey('ctrl', 'shift', 'esc')
 
-# # Função screenshot
-# pyautogui.screenshot('captura_tela.png')
+# Função screenshot
+pyautogui.screenshot('captura_tela.png')
 
 # Localização através de imagem (locateOnScreen)
 # O print para salvar a imagem de referência pode ser feito de maneira manual ou então através do aplicativo Flameshot ou ferramenta de captura
